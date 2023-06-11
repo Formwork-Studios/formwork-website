7beec2dd6c5e374e7cb31a91d6dc88c94dc4430f
mikey <mforest@protonmail.com>
2023-06-11 08:01:35 -0400

    updates

--- components/section/Info.vue
+++ components/section/Info.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="grid grid-cols-2 text-black bg-white-300 text-xl w-full p-4 gap-4 ">
     <!-- First Main Column -->
-    <div class="col-span-1 flex flex-col h-full relative">
-      <h2 class="font-bold text-black text-6xl uppercase">A website is a machine for information and engagement</h2>
+    <div class="col-span-1 flex flex-col h-full relative space-y-8">
+      <h2 class="font-bold text-black text-6xl uppercase">A website is a machine for information and delight</h2>
 
-      <div class="p-20 text-2xl text-black-300 text-right w-full ml-auto"><div class="ml-auto max-w-[40ch]">We must always create with the intention of reaching a goal. Digital Media is no different. There is a two-way road between knowing what you want and what the user wants.</div></div>
-<div class="inline">Read More <i-mdi-chevron-double-right class="inline" /> </div>
+      <div class="text-2xl text-black-300">
+        <div>
+          <p class="py-4">Far from being simply functional or purely aesthetic, a website serves as a conduit for both information and pleasure. Its design should be intuitive, effortlessly guiding users while also predicting their needs by presenting the right information at the right moment. Consequently, a website is a mechanism that delivers outcomes not only visually but also socially. It may persuade a user to make a purchase, join a community, 
+            or share a fascinating article with their circle. To manifest this tangible impact, however, a website must intertwine the informative with the delightful..</p> 
+          <p class="pb-4">The esteemed French architect Le Corbusier once proclaimed, "The house is a machine for living in." While a website may not seem to fulfill such a lofty objective as providing a 'habitat for living,' in the digital era, it contends for a respectable position on the importance hierarchy, rivaling physical living spaces. It suggests that information and its organization have become crucial components of our modern existence.</p>
+         <p class="">In every creation process, the intent should be to achieve a defined goal. The realm of Digital Media is no exception. It involves navigating a two-way path, balancing what the creator wants to convey and what the user seeks to discover.</p>
+        </div>
+      </div>
+      <div class="inline readmore">Read More <i-mdi-chevron-double-right class="inline chevron" /> </div>
       <div class="flex-grow"></div>
     </div>
     <!-- Second Main Column -->
@@ -31,4 +38,16 @@
 </template>
 
 <style scoped>
+.readmore {}
+
+.chevron {
+  animation: move 1.5s infinite; /* adjust the 1s value for the speed of the movement */
+  display: inline-block;
+}
+
+@keyframes move {
+  0% {transform: translateX(0);}
+  50% {transform: translateX(5px);} /* adjust the 10px value for the distance of the movement */
+  100% {transform: translateX(0);}
+}
 </style>
--- components/ui/Footer.vue
+++ components/ui/Footer.vue
@@ -1,37 +1,45 @@
 <template>
   <div class="bg-black text-white py-8">
-    <div class="container mx-auto flex justify-between items-start">
-      <div class="flex">
+    <div class="mx-20 flex justify-between">
+      <div class="flex grow">
         <div class="mr-20">
           <img src="formwork_color.png" alt="Formwork logo" class="h-40"/>
         </div>
         <div v-for="section in sections" :key="section.title" class="mr-20">
-          <h3 class="font-bold mb-2 border-b border-opacity-50">{{section.title}}</h3>
+          <h3 class="font-bold mb-2 border-b border-opacity-50 uppercase">{{section.title}}</h3>
           <ul v-if="section.links" :class="{ 'column-list': section.links.length > 5 }">
             <li v-for="link in section.links" :key="link.name"><a :href="link.url" class="text-white-500 hover:text-white-300 hover:text-white">{{link.name}}</a></li>
           </ul>
         </div>
-      </div>
-      <div class="w-1/4 flex flex-row text-xl gap-5">
-        <a href="https://www.pinterest.com" target="_blank">
-          <i-mdi-pinterest />
-        </a>
-        <a href="https://www.twitter.com" target="_blank">
-          <i-mdi-twitter />
-        </a>
-        <a href="https://www.linkedin.com" target="_blank">
-          <i-mdi-linkedin />
-        </a>
+        <div id="lastcol" class="flex grow flex-col h-full justify-between">
+  <div class="flex flex-row justify-end space-x-4 text-xl">
+    <a href="https://www.pinterest.com" target="_blank">
+      <i-mdi-pinterest />
+    </a>
+    <a href="https://www.twitter.com" target="_blank">
+      <i-mdi-twitter />
+    </a>
+    <a href="https://www.linkedin.com" target="_blank">
+      <i-mdi-linkedin />
+    </a>
+  </div>
+  <div class="text-right h-full flex flex-col justify-end text-sm text-stone-400">
+    <p class="mt-auto">© 2023 Formwork Studios. All rights reserved.</p>
+  </div>
+</div>
+
       </div>
     </div>
   </div>
 </template>
+
 
 <style scoped>
 .column-list {
   column-count: 2;
   column-gap: 40px;
 }
+
 </style>
 
 <script>
