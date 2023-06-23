fbf97c02ede6fa3bf680bc7a9b2f8323a5065f21
mikey <mforest@protonmail.com>
2023-06-23 06:14:53 -0400

    blog updates

--- pages/posts/website-machine.vue
+++ pages/posts/website-machine.vue
@@ -7,13 +7,13 @@ definePageMeta({
 <template>
   <div class="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 blog py-40">
     <!-- Header Image -->
-    <img class="w-full h-64 object-cover" src="/Unite-dhabitation-Marseille_C.-Emden.jpg"
-      alt="Blog post header image">
+    <img class="w-full h-64 object-cover" src="/Unite-dhabitation-Marseille_C.-Emden.jpg" alt="Blog post header image">
     <span class="pt-1.5 pb-7 inline-block text-white-800 text-base">The Unite d'habitation in Marseille, an example of Le
       Corbusier's brutalist architecture.</span>
 
     <!-- Blog post title -->
-    <h1 class="mt-4 mb-8 text-3xl font-bold">"A Website is a Machine for Information and Delight": Brutalism and UI/UX Design
+    <h1 class="mt-4 mb-8 text-3xl font-bold">"A Website is a Machine for Information and Delight": Brutalism and UI/UX
+      Design
     </h1>
 
     <div class="leading-loose text-lg">
@@ -28,40 +28,42 @@ definePageMeta({
         flourishes typical of classical architecture. Instead, Architects of that time embraced Le Corbusier's
         perspective, treating homes as what they fundamentally are —
         <a href="https://www.open.edu/openlearn/history-the-arts/history/heritage/le-corbusier" target="_blank"
-          class="border-b-2 border-opacity-50 hover:border-opacity-100 border-primary">machines for living in</a>.</p>
+          class="border-b-2 border-opacity-50 hover:border-opacity-100 border-primary">machines for living in</a>.
+      </p>
 
       <p>The radical shift towards functionality in architecture embodied in the Modernist movement was pushed to an
         extreme in Brutalism — a branch of its parent philosophy — by Le Corbusier himself. The proponents of this
-        movement championed the use of raw concrete and the creation of imposing structures, forgoing decorative embellishments.
+        movement championed the use of raw concrete and the creation of imposing structures, forgoing decorative
+        embellishments.
         This ensured that buildings were understood and <i>felt</i> for
         what they were, in their stark utilitarian essence, rather than designing under a framework that prioritized
         Romantic ideals and human emotions.
         In this way, Brutalist architects and planners were able to discard historical baggage, and begin their work of
-        rebuilding again from a blank slate — of concrete or Béton Brut, of course.</p>
+        rebuilding again from a blank slate — of Béton Brut, of course.</p>
 
       <p>Le Corbusier's vision was brought to life with his seminal work <a
           href="https://www.archdaily.com/85971/ad-classics-unite-d-habitation-le-corbusier" target="_blank"
-          class="border-b-2 border-opacity-50 hover:border-opacity-100 border-primary">Unite d'Habitation</a>,
+          class="border-b-2 border-opacity-50 hover:border-opacity-100 border-primary">Unite d'Habitation</a>, a housing complex in Marseille, France,
         finished in 1952 against grim and tumultuous backdrop of the post-WW2 era.
-        His dictum, "A house is a machine for living in," encapsulated the utilitarian spirit that fueled Brutalist
-        architecture and the push for an architecture that could address some of the
+        His dictum, "A house is a machine for living in," encapsulated the utilitarian spirit that fueled Brutalist 
+        and the push for an architecture that could address some of the
         worlds most pressing issues. In this sense, Brutalism was more than a mere
         stylistic preference; it was an emblem of shifting social and political current. The Brutalist movement gained
         prominence during a period marked by ideological
         tug-of-war between the world's two dominant powers, the Soviet Union and the United States. Both were trying to
-        promote their vision for a world emerging from the ashes
-        of unprecedented devastation, displacement, and upheaval. In this chaotic period, societal focus shifted toward
+        promote their vision for a world emerging from the ashes of ww2. In this chaotic period, societal focus shifted toward
         meeting people's basic needs through mass housing, public infrastructure, and affordability. Amidst these
-        conditions,
-        Brutalism stood out. Even with its fervent emphasis on function, however, Brutalism didn’t entirely sideline
+        conditions, Brutalism stood out. Even with its fervent emphasis on function, however, Brutalism didn’t entirely sideline
         aesthetics, discovering a unique beauty in the rawness of, sometimes towering,
         exposed structures and materials.</p>
 
       <h2 class="text-2xl capitalize pt-8 pb-4">Translating Brutalism in the Digital Era</h2>
 
       <p>
-        Elements of Brutalist design can be discerned in the works and writings of several Modernist thinkers.
-        In his 1908 essay "<a href="https://designmanifestos.org/adolf-loos-ornament-and-crime/" target="_blank" class="border-b-2 border-opacity-50 hover:border-opacity-100 border-primary">Ornament and Crime</a>," Alfred Loos famously wrote, "The urge to ornament one's face and
+        Elements of Brutalist design can be seen in the works and writings of several earlier Modernist thinkers.
+        In his 1908 essay "<a href="https://designmanifestos.org/adolf-loos-ornament-and-crime/" target="_blank"
+          class="border-b-2 border-opacity-50 hover:border-opacity-100 border-primary">Ornament and Crime</a>," Alfred
+        Loos famously wrote, "The urge to ornament one's face and
         everything within one's
         reach is a primitive impulse." He went on to state, "The evolution of culture marches with the elimination of
         ornament from
@@ -84,7 +86,7 @@ definePageMeta({
         seems to lack the foundational philosophical drive that
         underpinned the original movement. In many ways, neo-Brutalism appears to replicate some of the very aspects
         Brutalism sought to challenge, aspects that would likely have
-        been criticized by Alfred Loos. If the aim of Brutalism is to strip down a structure toits essence and highlight
+        been criticized by Alfred Loos. If the aim of Brutalism is to strip down a structure to its essence and highlight
         the strength of its raw material, then Brutalism in web
         design should reject the urge to ornament with aesthetic trends such as scrolling marquees and vertical text.
         Instead, it should aim to reveal the fundamental characteristics
@@ -96,18 +98,20 @@ definePageMeta({
       </div>
 
       <p>However, the reality is that not everyone desires to delve into the nuts and bolts or engage with raw materials.
-        Often, simplicity and functionality suffice. This is why, in my opinion,
-        Modernism continues to hold an enduring appeal. While Brutalism acts as a necessary counterbalance that keeps
-        Modernism anchored in the present, it tends to be most impactful
-        when used sparingly or on the periphery.</p>
+        Often, simplicity and functionality suffice. For this reason, Modernism continues to hold an enduring appeal. 
+        While Brutalism acts as a necessary counterbalance that keeps Modernism anchored on firm ground, it is simply too aggressive
+        to be used in most use-cases.</p>
 
-      <p>In a vein similar to Brutalist principles, contemporary web design must grapple with the interplay between
-        functionality and aesthetics.
+      <p>Contemporary UI/UX design must then allow for form to flow from function, however, with the understanding that the function of a website
+        is to first convey information or provide tools, while, as at same time, doing so in a way that is delightful or, at least, keeps the user
+        engaged in an ethical manner. 
         The concept of a website as a machine for information and delight echoes Le Corbusier's idea of a house being a
         machine for living in. Fundamentally,
         both Brutalism and web design strive to strike a balance between the practical and the pleasurable, the essential
         and the aesthetic. Keeping this in mind,
-        we can refer to Dieter Rams' "<a href="https://www.vitsoe.com/us/about/good-design" target="_blank" class="border-b-2 border-opacity-50 hover:border-opacity-100 border-primary">Ten Principles for Good Design</a>" as a guide:
+        we can refer to Dieter Rams' "<a href="https://www.vitsoe.com/us/about/good-design" target="_blank"
+          class="border-b-2 border-opacity-50 hover:border-opacity-100 border-primary">Ten Principles for Good Design</a>"
+        as a guide:
       </p>
 
       <ul class="list-none pl-10">
@@ -132,7 +136,7 @@ definePageMeta({
         <li class="pb-2 pt-1 relative"><span class="font-bold">Good design is as little design as possible:</span> It
           focuses on the essential aspects of a product and eliminates the superfluous.</li>
       </ul>
-  
+
 
 
       <p>With these principles in mind, it's crucial not to become overly preoccupied with theory. Sometimes, people
@@ -145,7 +149,8 @@ definePageMeta({
 </template>
 
 <style scoped>
-p, ul {
+p,
+ul {
   margin: 0px 0px 20px 0px;
 }
 
