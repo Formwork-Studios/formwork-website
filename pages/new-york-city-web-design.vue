120cd0f2df6969bb8382bb19d52d480b789791db
mikey <mforest@protonmail.com>
2023-08-18 16:06:56 -0400

    Working on SEO meta

---
+++ pages/new-york-city-web-design.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="h-screen w-full flex justify-center align-middle">
+    <h1>FUGGEDABOUTIT</h1>
+    <p>YOU NEED A WEBSITE? WE'LL MAKE A WEBSITE</p>
+  </div>
+</template>
+
+<style scoped></style>
